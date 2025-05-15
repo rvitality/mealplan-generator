b4934c239287032d1c480e15ff9e8ccead29eb49
rvitality <vital.renzjohn@gmail.com>
2025-05-15 21:31:21 +0800

    Migrate prisma with Profile model and add create profile route

---
+++ app/api/create-profile/route.ts
@@ -0,0 +1,43 @@
+import { prisma } from "@/lib/prisma";
+import { currentUser } from "@clerk/nextjs/server";
+import { NextRequest, NextResponse } from "next/server";
+
+export async function POST(request: NextRequest) {
+    try {
+        const clerkUser = await currentUser();
+
+        if (!clerkUser) {
+            return NextResponse.json({ error: "User not found in Clerk." }, { status: 404 });
+        }
+
+        const email = clerkUser?.emailAddresses[0].emailAddress;
+
+        if (!email) {
+            return NextResponse.json({ error: "User does not have an email address." }, { status: 400 });
+        }
+
+        const existingProfile = await prisma.profile.findUnique({
+            where: { userId: clerkUser.id },
+        });
+
+        if (existingProfile) {
+            return NextResponse.json({ message: "Profile already exists." });
+        }
+
+        await prisma.profile.create({
+            data: {
+                userId: clerkUser.id,
+                email,
+                subscriptionActive: false,
+                subscriptionTier: null,
+                stripeSubscriptionId: null,
+            },
+        });
+
+        console.log(`Prisma profile created for user: ${clerkUser.id}`);
+        return NextResponse.json({ message: "Profile created successfully." }, { status: 201 });
+    } catch (err) {
+        console.error("Error in create-profile API:", err);
+        return NextResponse.json({ error: "Internal Server Error." }, { status: 500 });
+    }
+}
---
+++ app/components/react-query-client-provider.tsx
@@ -0,0 +1,11 @@
+// app/client-provider.tsx
+"use client";
+
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactNode, useState } from "react";
+
+export default function ClientProvider({ children }: { children: ReactNode }) {
+    const [queryClient] = useState(() => new QueryClient());
+
+    return <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>;
+}
